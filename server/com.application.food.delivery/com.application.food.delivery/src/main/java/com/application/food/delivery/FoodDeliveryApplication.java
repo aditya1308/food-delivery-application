a683f13396769eaf462911d77ab4c90401fe3bdd
Aditya Prasad <asmarty2016@gmail.com>
2025-11-18 23:46:13 +0530

    FDA-5: Code cleanup

--- server/com.application.food.delivery/com.application.food.delivery/src/main/java/com/application/food/delivery/FoodDeliveryApplication.java
+++ server/com.application.food.delivery/com.application.food.delivery/src/main/java/com/application/food/delivery/FoodDeliveryApplication.java
@@ -1,15 +1,27 @@
 package com.application.food.delivery;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class FoodDeliveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
